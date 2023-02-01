253ac51fddedd7d9ea0e9cb1052ff875b72261d5
Mykhailo <toronto2v2@icloud.com>
2023-02-01 12:56:24 +0200

    modified component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,50 +7,79 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   password: string = '';
-  fullInput: boolean = false;
+  emptyInput: boolean = false;
   easy: boolean = false;
   medium: boolean = false;
   hard: boolean = false;
 
-  onInputChange() {
-    if (this.password.length > 0 && this.password.length < 8) {
-      this.fullInput = true;
-    } else {
-      this.fullInput = false;
-    }
 
-    if (
-      this.password.length >= 8 &&
-      (this.password.match(/^\d+$/g) ||
-        this.password.match(/^[a-zA-Z]+$/g) ||
-        this.password.match(/[^\d\sa-zA-Z]/gm))
-    ) {
-      this.easy = true;
-    } else {
-      this.easy = false;
-    }
+  onInputChange(str: string): void{
+    const words = /[a-z]/gi.test(str)
+    const digits = /\d/gi.test(str)
+    const simb = !!str.replace(/[a-z\d]/gi, '')
+    const length = str.length
 
-    if (
-      this.password.length >= 8 &&
-      (this.password.match(/(?=.*[0-9])(?=.*[a-zA-Z])/g) ||
-        this.password.match(/(?=.*[a-zA-Z])(?=.*[^\d\sa-zA-Z])/g) ||
-        this.password.match(/(?=.*[0-9])(?=.*[^\d\sa-zA-Z])/g))
-    ) {
+    this.emptyInput = length > 0 && length < 8? this.emptyInput = true : this.emptyInput = false;
+
+    this.easy = length >= 8 && (words || digits || simb) ? this.easy = true : this.easy = false ;
+
+    if(length >= 8 && (words && digits || words && simb || digits && simb)){
       this.easy = false;
       this.medium = true;
-    } else {
+    }else{
       this.medium = false;
     }
 
-    if (
-      this.password.length >= 8 &&
-      this.password.match(/(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[^\d\sa-zA-Z])/g)
-    ) {
+    if(length >= 8 && words && digits && simb){
       this.easy = false;
       this.medium = false;
       this.hard = true;
-    } else {
+    }else{
       this.hard = false;
     }
+
   }
+
+  // onInputChange() {
+  //   if (this.password.length > 0 && this.password.length < 8) {
+  //     this.fullInput = true;
+  //   } else {
+  //     this.fullInput = false;
+  //   }
+
+  //   if (
+  //     this.password.length >= 8 &&
+  //     (this.password.match(/^\d+$/g) ||
+  //       this.password.match(/^[a-zA-Z]+$/g) ||
+  //       this.password.match(/[^\d\sa-zA-Z]/gm))
+  //   ) {
+  //     this.easy = true;
+  //   } else {
+  //     this.easy = false;
+  //   }
+
+  //   if (
+  //     this.password.length >= 8 &&
+  //     (this.password.match(/(?=.*[0-9])(?=.*[a-zA-Z])/g) ||
+  //       this.password.match(/(?=.*[a-zA-Z])(?=.*[^\d\sa-zA-Z])/g) ||
+  //       this.password.match(/(?=.*[0-9])(?=.*[^\d\sa-zA-Z])/g))
+  //   ) {
+  //     this.easy = false;
+  //     this.medium = true;
+  //   } else {
+  //     this.medium = false;
+  //   }
+
+  //   if (
+  //     this.password.length >= 8 &&
+  //     this.password.match(/(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[^\d\sa-zA-Z])/g)
+  //   ) {
+  //     this.easy = false;
+  //     this.medium = false;
+  //     this.hard = true;
+  //   } else {
+  //     this.hard = false;
+  //   }
+  // }
 }
+
